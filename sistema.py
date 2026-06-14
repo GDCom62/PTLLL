c557ece655681288017487485da1f291912adea6
William de Aguiar Campos <138343439+GDCom62@users.noreply.github.com>
2026-06-14 14:11:34 -0300

    Adjust fixed position and size of logo in CSS

--- sistema.py
+++ sistema.py
@@ -23,11 +23,11 @@ if img_marca_b64:
         <style>
         .marca-fixa {
             position: fixed;
-            bottom: 10px;
-            right: 10px;
+            bottom: 3px;
+            right: 3px;
             z-index: 9999;
             opacity: 0.4; /* Deixa o logo semi-transparente estilo marca d'água */
-            max-width: 25px; /* Reduzido para ficar bem pequeno e discreto no canto */
+            max-width: 55px; /* Reduzido para ficar bem pequeno e discreto no canto */
             pointer-events: none;
         }
         </style>
