1b101dbe378a72d44a8f3fd58f695960d7181836
William de Aguiar Campos <138343439+GDCom62@users.noreply.github.com>
2026-06-21 22:45:52 -0300

    Fix formatting of comment in main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ menu = st.sidebar.radio("Navegar para:", [
 ])
 
 # ==========================================
-# 1. PÁGINA:  CADASTRO E EDIÇÃO
+# 1. PÁGINA: CADASTRO E EDIÇÃO
 # ==========================================
 if menu == "📋 Cadastro & Edição de Máquinas":
     st.header("📋 Gerenciamento de Máquinas e Equipamentos")
